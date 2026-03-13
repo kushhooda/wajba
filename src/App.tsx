1c7ecb135bf89cb1afc2339612a97453c71ddc2f
Kush Hooda <kush9hooda@gmail.com>
2026-03-13 19:35:55 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import ProtectedRoute from '@/components/ProtectedRoute'
+import LandingPage from '@/pages/LandingPage'
+import AuthPage from '@/pages/AuthPage'
+import OnboardingPage from '@/pages/OnboardingPage'
+import DashboardPage from '@/pages/DashboardPage'
+import MenuPage from '@/pages/MenuPage'
+import BillPage from '@/pages/BillPage'
+import NotFound from '@/pages/NotFound'
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<LandingPage />} />
+    <Route path="/login" element={<AuthPage />} />
+    <Route path="/signup" element={<AuthPage />} />
+    <Route path="/onboarding" element={<ProtectedRoute><OnboardingPage /></ProtectedRoute>} />
+    <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+    <Route path="/menu/:slug" element={<MenuPage />} />
+    <Route path="/bill/:orderId" element={<BillPage />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+)
+
+export default App
