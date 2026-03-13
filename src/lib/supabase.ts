0014b84bcc25f61ed0d25a70df7cb598f3ad5ae6
Kush Hooda <kush9hooda@gmail.com>
2026-03-13 20:57:02 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.VITE_SUPABASE_URL as string
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+const url = 'https://zmphaechgiujzdfejzvo.supabase.co'
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptcGhhZWNoZ2l1anpkZmVqenZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTE1MjMsImV4cCI6MjA4ODk4NzUyM30.Ub95IvVo1Kfupr1vKDHGvnadzn90s4E7k-G9fOjXq44'
 
 export const supabase = createClient(url, key)
